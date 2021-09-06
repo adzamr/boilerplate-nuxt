ba59d7c3af4b73b6e0c3a991323be86e48b5e8c4
adzharamrullah <adzharamrullah@gmail.com>
2021-09-06 14:15:55 +0700

    logout with send refresh token to backend

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
+    <button @click="accessUser">
+      Akses user
+    </button>
     <h1>
       Welcome to Home
     </h1>
-    <nuxt-link to="/about">About</nuxt-link>
+    <nuxt-link to="/about">
+      About
+    </nuxt-link>
     <button @click="logout">
       Logout
     </button>
@@ -13,7 +18,13 @@
 <script>
 export default {
   methods: {
+    async accessUser () {
+      const res = await this.$axios.get('/user')
+      console.log(res.data)
+    },
     async logout () {
+      const refreshToken = this.$auth.strategy.refreshToken.get()
+      await this.$axios.post('/logout', { refresh_token: refreshToken })
       await this.$auth.logout()
     }
   }
